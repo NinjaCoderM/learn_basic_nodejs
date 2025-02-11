226d7f805a2fb67ad15dddf7a2acb9bc162042da
wienm <martin.scheibelreiter@chello.at>
2025-02-11 11:23:06 +0100

    Objekt erster Klasse

--- js/app.js
+++ js/app.js
@@ -4,4 +4,13 @@ const {testExportExampleAdd} = require("./functionModule");
 
 console.log(testExportExampleAdd(1,3));
 
+function Person(name, alter){
+  let vname = name;
+  let valter = alter;
+  function getPersonInfo(){
+    return vname + ' ' + valter;
+  }
+  return getPersonInfo();
+}
 
+console.log(Person("Ernst", 22))
