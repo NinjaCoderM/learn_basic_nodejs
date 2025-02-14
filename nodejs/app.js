17d628baaf9782bc5729aeffdd92b3ebb2625590
wienm <martin.scheibelreiter@chello.at>
2025-02-14 20:46:30 +0100

    arrow-Schreibweise

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,26 +2,16 @@
 
 const fs = require("node:fs").promises;
 
-async function readFile(filePath, encoding){
-  try{
+const readFile2 = async (filePath, encoding) => {
+  try {
     const data = await fs.readFile(filePath, encoding);
     console.log(data.toString());
-  } catch(error){
+  } catch (error) {
     console.log("Es gab einen Fehler: " + error.message);
   }
-}
+};
 
-async function readFile2(filePath, encoding){
-  try{
-    let data;
-    fs.readFile(filePath, encoding)
-      .then(value=>{data = value.toString();console.log(data.toString())});
-  } catch(error){
-    console.log("Es gab einen Fehler: " + error.message);
-  }
-}
-
-async function writeFile(filePath, text, options){
+const writeFile = async (filePath, text, options) => {
   try{
    await fs.writeFile(filePath, text ,options);
   } catch(error){
@@ -29,19 +19,25 @@ async function writeFile(filePath, text, options){
   }
 }
 
-async function addItem(filePath, name, anzahl, preis, options){
-  try{
+const addItem = async (filePath, name, anzahl, preis, options) => {
+  try {
     const item = `\n${name}, ${anzahl}, ${preis}`;
-    writeFile(filePath, item, options);
-  } catch(error){
+    await writeFile(filePath, item, options);
+  } catch (error) {
     console.log("Es gab einen Fehler: " + error.message);
   }
-}
+};
+
+
+let textArray = [["A", "2", "2€"], ["B", "3", "3€"], ["C", "4", "4€"]];
 
 (async function(){ //wegen Reihenfolge mit await pro Zeile
-  await writeFile('text.txt', 'Name, Anzahl, Preis', 'utf8');
+  await writeFile('text.txt', 'Name, Anzahl, Preis', { encoding: "utf8" });
   await addItem('text.txt', 'Apfel', '4', '4€', {flag: "a", encoding: "utf8"});
   await addItem('text.txt', 'Apfel+', '4', '40€', {flag: "a", encoding: "utf8"});
+  for (const item of textArray) {
+    await addItem('text.txt', ...item, {flag: "a", encoding: "utf8"});
+  }
   await readFile2('text.txt', 'utf8');
 })();
 
