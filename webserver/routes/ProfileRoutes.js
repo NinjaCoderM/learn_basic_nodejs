787aa54a9b634fa734f878ac3340ef8071bf4f7a
wienm <martin.scheibelreiter@chello.at>
2025-02-15 19:41:52 +0100

    GET, Post zusammenfassen

--- webserver/routes/ProfileRoutes.js
+++ webserver/routes/ProfileRoutes.js
@@ -6,6 +6,16 @@ ProfileRoutes.get('/data', (req, res) => {
   res.send(`Data `)
 })
 
+ProfileRoutes.route("/userdata").get((req, res) => {
+  res.send(`UserData get `)
+}).post((req, res) => {
+  res.send(`UserData post `)
+}).delete((req, res) => {
+  res.send(`UserData delete `)
+}).put((req, res) => {
+  res.send(`UserData put `)
+})
+
 ProfileRoutes.get('/:userId', (req, res) => {
   res.send(`Hello World, im Profil ` + req.params.userId )
 })
