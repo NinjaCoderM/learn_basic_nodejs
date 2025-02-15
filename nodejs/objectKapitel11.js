0d3e1eea373087d2f815e11efd12bd6ad893efda
wienm <martin.scheibelreiter@chello.at>
2025-02-15 14:26:06 +0100

    Vererbung

--- nodejs/objectKapitel11.js
+++ nodejs/objectKapitel11.js
@@ -36,3 +36,19 @@ class ClassCreditCard {
 let classCreditCard = new ClassCreditCard("215330021", "MM3");
 console.log(classCreditCard);
 console.log(classCreditCard.info());
+
+class PremiumCreditCard extends ClassCreditCard {
+  constructor(number, besitzer, bonus) {
+    super(number, besitzer); // Aufruf des Konstruktors der Elternklasse
+    this.bonus = bonus;
+  }
+
+  getBonus() {
+    return "Bonus points available: " + this.bonus;
+  }
+}
+
+const myPremiumCard = new PremiumCreditCard("215330021", "MM4", 5000);
+
+console.log(myPremiumCard.info());      // Geerbte Methode
+console.log(myPremiumCard.getBonus());
