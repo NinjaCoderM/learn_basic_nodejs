f5faf773778faf9edb00f59487d6e922bf127fa3
wienm <martin.scheibelreiter@chello.at>
2025-02-15 16:58:22 +0100

    schreibweise mit Pfeil statt function

--- webserver/app.js
+++ webserver/app.js
@@ -1,8 +1,8 @@
 "strict mode"
 import {server, PORT} from "./server.js"; //"type": "module" in package.json notwendig
 
-server.get('/', function (req, res) {
-  res.send('Hello World ...+++')
+server.get('/', (req, res) => {
+  res.send('Hello World ...+')
 })
 
 server.listen(PORT, ()=> console.log("Server ist gestartet auf PORT: " + PORT))
