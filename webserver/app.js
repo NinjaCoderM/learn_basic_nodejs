2cec52c2277b40db28e2b1c98aea1a5bba7e18f6
wienm <martin.scheibelreiter@chello.at>
2025-02-15 18:45:57 +0100

    res.redirect

--- webserver/app.js
+++ webserver/app.js
@@ -2,7 +2,7 @@
 import {server, PORT} from "./server.js"; //"type": "module" in package.json notwendig
 
 server.get('/', (req, res) => {
-  res.json({name: "Klaus", alter:44});
+  res.redirect("/profil");
 })
 
 server.post('/profil', (req, res) => {
