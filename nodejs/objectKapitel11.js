654105858a7660575e6732b06f82cfcc77ee3573
wienm <martin.scheibelreiter@chello.at>
2025-02-15 14:05:59 +0100

    Object / Constructor Function

--- nodejs/objectKapitel11.js
+++ nodejs/objectKapitel11.js
@@ -11,4 +11,13 @@ let myCard = CreditCard("215330021", "MM");
 console.log(myCard);
 console.log(myCard.info());
 
+function ConstCreditCard(number, besitzer) {
+  this.number = number;
+  this.besitzer = besitzer;
+  this.info = function(){return "Constructor Example Credit Card " + this.number + " from " + this.besitzer;};
+}
+
+let myConstCreditCard = new ConstCreditCard("215330021", "MM");
+console.log(myConstCreditCard);
+console.log(myConstCreditCard.info());
 
