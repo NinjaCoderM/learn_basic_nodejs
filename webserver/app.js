eb24de710c4d400bb7ed9f67a733aaef9210f0e2
wienm <martin.scheibelreiter@chello.at>
2025-02-15 16:52:14 +0100

    nodemon package.json -> npm run dev

--- webserver/app.js
+++ webserver/app.js
@@ -2,7 +2,7 @@
 import {server, PORT} from "./server.js"; //"type": "module" in package.json notwendig
 
 server.get('/', function (req, res) {
-  res.send('Hello World ...+')
+  res.send('Hello World ...+++')
 })
 
 server.listen(PORT, ()=> console.log("Server ist gestartet auf PORT: " + PORT))
